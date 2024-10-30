1dcd9a94d428dbe9b158d279a93bf5928b71d202
CakeDsx <philipvtn@gmail.com>
2024-10-30 23:40:14 +0100

    Update About.tsx

--- frontend/src/components/About.tsx
+++ frontend/src/components/About.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 const About: React.FC = () => {
     return (
-        <section id="about">
-            <h2>About Me</h2>
-            <p>Hello! My name is Philip and I am a Web Developer.</p>
-        </section>
-    );
-};
+            <section id="about">
+                <h2>About Me</h2>
+                <p>Hello! My name is Philip and I am a Web Developer.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta accusamus, quis minus tempora quibusdam optio, explicabo tenetur iure vero ipsum aut unde aliquam rerum recusandae saepe voluptatum atque nihil sit dolores eius maiores cupiditate omnis quae a. Quos sed pariatur rem reprehenderit ducimus fuga amet, minus, delectus nostrum, perspiciatis doloribus?
+                <h3>More about me</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam libero nihil dolore alias error excepturi quo temporibus sed esse quidem eaque omnis, corrupti ratione nisi exercitationem accusamus rem doloribus earum quibusdam illum, sit, recusandae eveniet praesentium commodi. Similique, iste recusandae quam eveniet cumque necessitatibus minima, atque quos voluptatem sint distinctio!</p>
+                </p>
+            </section>
+        );
+    };
+
 
 export default About;
